b2cf6281ea8837dffb54f851a9ada59809bfa263
chrischross <chris.klose@gmx.net>
2021-01-24 15:34:33 +0100

    upload

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import streamlit as st
 import numpy as np
 from streamlit_drawable_canvas import st_canvas
-from train import Net
+# from train import Net
 from scipy.ndimage.interpolation import zoom
 import os
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
